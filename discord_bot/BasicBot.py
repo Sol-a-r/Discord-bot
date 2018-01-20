2291c585b77f838122177eb24dba7fdcbcb6febf
Sol.a.r <33205264+Sol-a-r@users.noreply.github.com>
2018-01-19 18:11:47 -0600

    finally working

--- discord_bot/BasicBot.py
+++ discord_bot/BasicBot.py
@@ -4,9 +4,9 @@ import asyncio
 from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
-
+import time
 # Here you can modify the bot's prefix and description and wether it sends help in direct messages or not.
-client = Bot(description="", command_prefix=">", pm_help = True)
+client = Bot(description="", command_prefix="~", pm_help = True)
 
 # This is what happens everytime the bot launches. In this case, it prints information like server count, user count the bot is connected to, and the bot id in the console.
 # Do not mess with it because the bot can break, if you wish to do so, please consult me or someone trusted.
@@ -21,18 +21,15 @@ async def on_ready():
 
 # This is a basic example of a call and response command. You tell it do "this" and it does it.
 @client.command()
-async def ping(*args):
+async def info():
 
-	await client.say(":ping_pong: Pong!")
+	await client.say(" fuck off you think I actually have the time to put some actual work into this bot. HELL NO. I'm going to bed it's 11:38, and I've got school tomarrow. ")
+
+@client.command()
+async def kpop():
+	await client.say('K-Pop is a sin...')
+	time.sleep(2)
+	await client.say('You are going to hell')
 
 
 client.run('NDAzNTg4OTA4Mzc5NjY4NDgx.DULcKg.jrLsEaxAI3jqmL-CtFabTVgFk_k')
-
-# Basic Bot was created by Habchy#1665
-# Please join this Discord server if you need help: https://discord.gg/FNNNgqb
-# Please modify the parts of the code where it asks you to. Example: The Prefix or The Bot Token
-# This is by no means a full bot, it's more of a starter to show you what the python language can do in Discord.
-# Thank you for using this and don't forget to star my repo on GitHub! [Repo Link: https://github.com/Habchy/BasicBot]
-
-# The help command is currently set to be Direct Messaged.
-# If you would like to change that, change "pm_help = True" to "pm_help = False" on line 9.
